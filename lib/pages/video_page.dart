64c1be59bb2e2e4a7a911f733109ff3afd84080d
young315 <young315@knu.ac.kr>
2020-11-15 17:30:04 +0900

    load video

--- lib/pages/video_page.dart
+++ lib/pages/video_page.dart
@@ -1,16 +1,17 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 
 class VideoPage extends StatelessWidget {
   static const routeName = "/video";
+
   @override
   Widget build(BuildContext context) {
     double deviceWidth = MediaQuery.of(context).size.width;
@@ -35,67 +36,32 @@ class VideoPage extends StatelessWidget {
             ],
             backgroundColor: Hexcolor('#fdfdfd'),
         ),
-        body: SafeArea(
-          child : Column(
-            children: <Widget>[
-              SizedBox(
-                  width:deviceWidth,
-                  height:179,
-                  child: const DecoratedBox(
-                    decoration: const BoxDecoration(
-                      color: Color.fromRGBO(255,243,196,1.0),
+        body: Column(
+          children: <Widget> [
+            SafeArea(
+              child : Column(
+                children: <Widget>[
+                  SizedBox(
+                    width:deviceWidth,
+                    height:179,
+                    child: const DecoratedBox(
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(255,243,196,1.0),
+                      ),
                     ),
                   ),
+                ],
               ),
-              Expanded(
-                child: GridView.count(
-                  padding: const EdgeInsets.all(16.0),
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 5.0,
-                    crossAxisSpacing: 5.0,
-                    children: List.generate(20,(index){
-                      return Stack(
-                        children:<Widget>[
-                          Container(
-                            color: Color.fromRGBO(178, 234,186,1.0),
-                          ),
-                          Positioned(
-                            child: Container(
-                              margin: const EdgeInsets.only(left: 78.0, top: 98.0),
-                              child: Text("00:00",
-                              style: TextStyle(fontSize: 12,
-                                  color: Colors.black),
-                              ),
-                            ),
-                          )
-                        ]
-                      );
-                    })
-                )
-              ),
-              Positioned(
-                  child: Container(
-                    height: 36,
-                    width: 80,
-                    alignment: Alignment.center,
-                    decoration: const BoxDecoration(
-                      color: Color.fromRGBO(255,255,255,8.0),
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                    ),
-                    child: Text(
-                        "앨범",
-                        style: TextStyle(
-                          color: Hexcolor('#333333'), fontSize: 14,
-                        )
-                    ),
-                  )
-              ),
-          ],
-        )
+            ),
+            Expanded(
+              child: Gallery(),
+            ),
+          ]
         )
     );
   }
 }
+
 class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -108,6 +74,183 @@ class Logo extends StatelessWidget {
           width: 3,
         ), // The size box provides an immediate spacing between the widgets
       ],
+    );
+  }
+}
+
+class Gallery extends StatefulWidget {
+  @override
+  _GalleryState createState() => _GalleryState();
+}
+
+class _GalleryState extends State<Gallery> {
+  // This will hold all the assets we fetched
+  List<AssetEntity> assets = [];
+
+  @override
+  void initState() {
+    _fetchAssets();
+    super.initState();
+  }
+
+  _fetchAssets() async {
+    // Set onlyAll to true, to fetch only the 'Recent' album
+    // which contains all the photos/videos in the storage
+    final albums = await PhotoManager.getAssetPathList(onlyAll: true, type:RequestType.video);
+    final recentAlbum = albums.first;
+
+    // Now that we got the album, fetch all the assets it contains
+    final recentAssets = await recentAlbum.getAssetListRange(
+      start: 0, // start at index 0
+      end: 1000000, // end at a very big index (to get all the assets)
+    );
+
+    // Update the state and notify UI
+    setState(() => assets = recentAssets);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          // A grid view with 3 items per row
+          crossAxisCount: 3,
+        ),
+        itemCount: assets.length,
+        itemBuilder: (_, index) {
+          return AssetThumbnail(asset: assets[index]);
+        },
+    );
+  }
+}
+
+class AssetThumbnail extends StatelessWidget {
+  const AssetThumbnail({
+    Key key,
+    @required this.asset,
+  }) : super(key: key);
+
+  final AssetEntity asset;
+
+  @override
+  Widget build(BuildContext context) {
+    // We're using a FutureBuilder since thumbData is a future
+    return FutureBuilder<Uint8List>(
+      future: asset.thumbData,
+      builder: (_, snapshot) {
+        final bytes = snapshot.data;
+        // If we have no data, display a spinner
+        if (bytes == null) return CircularProgressIndicator();
+        // If there's data, display it as an image
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) {
+                    // if it's not, navigate to VideoScreen
+                    return VideoScreen(videoFile: asset.file);
+                  }
+              ),
+            );
+          },
+          child: Stack(
+            children: [
+              // Wrap the image in a Positioned.fill to fill the space
+              Positioned.fill(
+                child: Image.memory(bytes, fit: BoxFit.cover),
+              ),
+              // Display a Play icon if the asset is a video
+              if (asset.type == AssetType.video)
+                Center(
+                  child: Container(
+                    color: Colors.blue,
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class VideoScreen extends StatefulWidget {
+  const VideoScreen({
+    Key key,
+    @required this.videoFile,
+  }) : super(key: key);
+
+  final Future<File> videoFile;
+
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  VideoPlayerController _controller;
+  bool initialized = false;
+
+  @override
+  void initState() {
+    _initVideo();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  _initVideo() async {
+    final video = await widget.videoFile;
+    _controller = VideoPlayerController.file(video)
+    // Play the video again when it ends
+      ..setLooping(true)
+    // initialize the controller and notify UI when done
+      ..initialize().then((_) => setState(() => initialized = true));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: initialized
+      // If the video is initialized, display it
+          ? Scaffold(
+        body: Center(
+          child: AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            // Use the VideoPlayer widget to display the video.
+            child: VideoPlayer(_controller),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Wrap the play or pause in a call to `setState`. This ensures the
+            // correct icon is shown.
+            setState(() {
+              // If the video is playing, pause it.
+              if (_controller.value.isPlaying) {
+                _controller.pause();
+              } else {
+                // If the video is paused, play it.
+                _controller.play();
+              }
+            });
+          },
+          // Display the correct icon depending on the state of the player.
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
+      )
+      // If the video is not yet initialized, display a spinner
+          : Center(child: CircularProgressIndicator()),
     );
   }
 }
